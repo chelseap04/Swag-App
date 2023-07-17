3b894f0b93678ae148b11425e242492705b95c0c
Chelsea Poppleton <chelseap@new.rr.com>
2023-07-17 13:42:53 -0500

    Initial Commit

---
+++ Swag App/Swag_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swag_AppApp.swift
+//  Swag App
+//
+//  Created by Chelsea Poppleton on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Swag_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
